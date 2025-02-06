c2de899f14026b408cc0408e3c62d6ecc18a13d7
Phucbeo710 <athanhphuc7102005@gmail.com>
2025-02-06 15:38:31 +0700

    for

--- b_3.cpp
+++ b_3.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a;
+    int n, sum =0,i=0;
+    cout << "Enter a number: ";
+    cin >> n;
+    while(i < n){
+        i++;
+        sum +=i;
+    }
+    cout << "Tong tu den " << n << " la: " << sum << endl;
+    return 0;
 }
---
+++ b_4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,sum = 0;
+    cout << "Enter a: ";
+    cin >> a;
+
+    for(int i = 1; i <= a; i++){
+        sum+=i;
+       
+        (sum == 55)? cout << "***\t" : cout << sum << "\t ";
+       
+    }
+    cout << "Tong tu 0 den " << a << " la: " << sum << endl;
+}
